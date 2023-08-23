4314c81abc887bf8f618bc4473e26ab50f9f2f75
Martins100-Tmd <Olaitanfabayo@gmail.com>
2023-08-23 12:51:07 +0000

    Caching Algorithms

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Task 1's module
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """Fifo class"""
+
+    def __init__(self):
+        BaseCaching.__init__(self)
+
+    def put(self, key, item):
+        """Add an item to cache"""
+        if item is None or key is None:
+            pass
+        else:
+            isInCache = self.cache_data.get(key)
+            cache_keys = list(self.cache_data.keys())
+            if len(cache_keys) >= BaseCaching.MAX_ITEMS and isInCache is not None:
+                self.cache_data.__delitem__(cache_keys[0])
+                self.cache_data.update({key: item})
+                print("DISCARD: {}".format(cache_keys[0]))
+            else:
+                self.cache_data.update({key: item})
+
+    def get(self, key):
+        """Get an item from cache"""
+        if self.cache_data.get(key) is not None:
+            return self.cache_data.get(key)
+        return None
+
